73ed02a7d216bd5c39c650e486b9f8d436df688f
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2023-07-21 00:21:34 -0300

    Initial Commit

---
+++ src/main/java/com/estudo/security/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.estudo.security.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String email;
+    private String password;
+}
