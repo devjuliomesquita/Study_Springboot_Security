5eba9bb78571878dea308cbdeaea5c8fee964800
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-24 14:25:44 -0300

    ApplicationConfig OK

---
+++ src/main/java/com/estudo/security/config/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.estudo.security.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(Customizer.withDefaults())
+
+                .csrf((csrf)-> csrf.disable())
+                .authorizeHttpRequests((authz)-> authz
+                        .requestMatchers().permitAll().anyRequest().authenticated())
+                .authenticationManager()
+                .addFilterBefore()
+                .
+
+
+        return http.build();
+    }
+}
